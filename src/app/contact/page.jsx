3cd7106ad28cce7f2d76948f283c62ab088f4588
joonseojung <joonseo1227@gmail.com>
2025-10-11 23:52:24 +0900

    fix: ContactPage 이메일 복사 기능 및 관련 스타일 제거

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,33 +1,14 @@
 'use client';
 import styles from '@/styles/pages/ContactPage.module.css';
-import {useState} from 'react';
-import {CheckmarkFilled, Copy} from '@carbon/icons-react';
 
 export default function ContactPage() {
-    const [copied, setCopied] = useState(false);
     const email = "joonseo1227@gmail.com";
-
-    const copyToClipboard = async () => {
-        try {
-            await navigator.clipboard.writeText(email);
-            setCopied(true);
-            setTimeout(() => setCopied(false), 2000);
-        } catch (err) {
-            console.error('텍스트 복사 실패: ', err);
-        }
-    };
 
     return (
         <div className={styles.contactPage}>
             <h1 className="titleText">Contact</h1>
             <div className={styles.emailContainer}>
                 <h2>{email}</h2>
-                <button
-                    className={`${styles.copyButton} ${copied ? styles.copied : ''}`}
-                    onClick={copyToClipboard}
-                >
-                    {copied ? <CheckmarkFilled size={24}/> : <Copy size={24}/>}
-                </button>
             </div>
         </div>
     );
