d26729adc768f1128337677d687e4abcdf8e2961
joonseojung <joonseo1227@gmail.com>
2025-12-08 19:02:24 +0900

    feat: rehype-slug 플러그인 추가 및 기존 코드 개선

--- src/components/MarkdownContent.jsx
+++ src/components/MarkdownContent.jsx
@@ -1,44 +1,15 @@
 "use client";
 
-import {useRef} from "react";
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
+import rehypeSlug from 'rehype-slug';
 
 export default function MarkdownContent({content}) {
-    // Create ref for heading counters
-    const headingCounterRef = useRef(0);
-
     return (
         <ReactMarkdown
             remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeRaw]}
-            components={{
-                h1: ({node, ...props}) => {
-                    const id = `heading-${headingCounterRef.current++}`;
-                    return <h1 id={id} {...props} />;
-                },
-                h2: ({node, ...props}) => {
-                    const id = `heading-${headingCounterRef.current++}`;
-                    return <h2 id={id} {...props} />;
-                },
-                h3: ({node, ...props}) => {
-                    const id = `heading-${headingCounterRef.current++}`;
-                    return <h3 id={id} {...props} />;
-                },
-                h4: ({node, ...props}) => {
-                    const id = `heading-${headingCounterRef.current++}`;
-                    return <h4 id={id} {...props} />;
-                },
-                h5: ({node, ...props}) => {
-                    const id = `heading-${headingCounterRef.current++}`;
-                    return <h5 id={id} {...props} />;
-                },
-                h6: ({node, ...props}) => {
-                    const id = `heading-${headingCounterRef.current++}`;
-                    return <h6 id={id} {...props} />;
-                }
-            }}
+            rehypePlugins={[rehypeRaw, rehypeSlug]}
         >
             {content || "내용이 없습니다."}
         </ReactMarkdown>
